c6437b3b22237b5bac6f1ddbc478f270f5305ab4
ross-spencer <all.along.the.watchtower2001+github@gmail.com>
2024-12-17 21:25:38 +0100

    Use context manager

--- tests/test_specific_issues.py
+++ tests/test_specific_issues.py
@@ -5,10 +5,11 @@ from freezegun import freeze_time
 from src.validation.validation import _check_timestamps, check_timestamps
 
 
-@freeze_time("2015-07-12T12:30:00Z")
 def test_freeze_gun():
     """Test freezegun."""
-    check_timestamps()
+
+    with freeze_time("2015-07-12T12:30:00Z"):
+        check_timestamps()
 
 
 @freeze_time("2015-07-12T12:30:00Z")
